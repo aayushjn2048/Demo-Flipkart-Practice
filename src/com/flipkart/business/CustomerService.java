7ed0d06026de164fc2dcd10f5d32b1aa8f8d1d95
Ayush <aayushjn.0248@gmail.com>
2022-01-21 12:02:05 +0530

    Second commit

--- src/com/flipkart/business/CustomerService.java
+++ src/com/flipkart/business/CustomerService.java
@@ -3,15 +3,36 @@
  */
 package com.flipkart.business;
 
+import com.flipkart.bean.Customer;
+
 /**
  * @author HP
  *
  */
 public class CustomerService {
 	//The service class implementing all the operations for creating, updating and deleting the customers
+	
+	//Customer customer = new Customer();
+	Customer myCustomers[] = new Customer[] {new Customer(), new Customer(), new Customer()};
+
+	
+	
 	public void createCustomer() {
 		
 		System.out.println("Customer is created");
+		//Add Dummy values in customer object
+		// First Record Insert
+		myCustomers[0].setCustomerId(101);
+		myCustomers[0].setCustomerName("Training");
+		myCustomers[0].setCustomerAddress("IBM");
+		// Second Record Insert
+		myCustomers[1].setCustomerId(102);
+		myCustomers[1].setCustomerName("Flipkart");
+		myCustomers[1].setCustomerAddress("Delhi");
+		// Third Record Insert
+		myCustomers[0].setCustomerId(103);
+		myCustomers[0].setCustomerName("JBL");
+		myCustomers[0].setCustomerAddress("Patna");
 	}
 	
 	public void updateCustomer() {
@@ -21,6 +42,12 @@ public class CustomerService {
 	public void listCustomer() {
 		
 		System.out.println("Customer is listed");
+		for(Customer  cust : myCustomers){
+			
+			  System.out.println("Customer Details---> id--> " +cust.getCustomerId() + "Name" +cust.getCustomerName() + "Address" +cust.getCustomerAddress());	
+				
+			}
+		
 	}
 	public void deleteCustomer() {
 		
